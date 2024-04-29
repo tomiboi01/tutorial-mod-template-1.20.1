9284b655eac919a95b1b63927a2715b41cf446f3
tomiboi01 <tomithespeedster@gmail.com>
2024-04-29 02:46:26 -0300

    Initial commit

---
+++ src/main/java/net/tomas/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.tomas.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
