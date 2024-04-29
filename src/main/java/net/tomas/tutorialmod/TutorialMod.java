a7ae66061e3a53df9d1e7ded9366872acd6d783e
tomiboi01 <tomithespeedster@gmail.com>
2024-04-29 03:43:47 -0300

    kkk

--- src/main/java/net/tomas/tutorialmod/TutorialMod.java
+++ src/main/java/net/tomas/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.tomas.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tomas.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!k");
+		ModItems.registerModItems();
 	}
 }
