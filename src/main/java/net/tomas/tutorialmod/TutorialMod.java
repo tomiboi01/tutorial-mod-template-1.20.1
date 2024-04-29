b2c0969308dd84af3bb00e4f119c9fe2496953ed
tomiboi01 <tomithespeedster@gmail.com>
2024-04-29 02:48:01 -0300

    kkk

--- src/main/java/net/tomas/tutorialmod/TutorialMod.java
+++ src/main/java/net/tomas/tutorialmod/TutorialMod.java
@@ -15,6 +15,6 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world!k");
 	}
 }
